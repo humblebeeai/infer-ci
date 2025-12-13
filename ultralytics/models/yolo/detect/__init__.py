137f9b7f2e68a6f7e947b264c9d62d8946454b71
azamjon-xusanov <azamjonxusanov.hbai@gmail.com>
2025-12-13 13:15:20 +0900

    remove ultralytics folder

--- ultralytics/models/yolo/detect/__init__.py
+++
@@ -1,8 +0,0 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from .predict import DetectionPredictor
-from .train import DetectionTrainer
-from .val import DetectionValidator
-from .ci_val import DetectionCIValidator
-
-__all__ = "DetectionPredictor", "DetectionTrainer", "DetectionValidator", "DetectionCIValidator"
